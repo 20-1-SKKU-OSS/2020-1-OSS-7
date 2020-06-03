f066ae0d0870c6486bc55578864623baf186a2c5
KES <kanges103802@gmail.com>
2020-06-03 22:51:15 +0900

    add client & server Python program

--- Python/other/client.py
+++ Python/other/client.py
@@ -1,14 +1,14 @@
 from socket import *
 
 socket_client = socket(AF_INET, SOCK_STREAM)
-socket_client.connect(('127.0.0.1',9876))
+socket_client.connect(('127.0.0.1', 9876))
 
-print("Connected")
+print('Connected')
 socket_client.send('message from client'.encode('utf-8'))
 
-print("Send Message")
+print('Send Message')
 
 data = socket_client.recv(1024)
-print('Received :',data.decode('utf-8'))
+print('Received : ', data.decode('utf-8'))
 
 socket_client.close()
